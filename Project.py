aed170f01e0d1d08acde02985d19487f8cd9bf8e
IH_ADMP <andredmpinto@gmail.com>
2022-03-05 13:12:10 +0000

    Initialize some files

---
+++ Project.py
@@ -0,0 +1,20 @@
+# libraries
+import pandas as pd
+import requests
+
+# read the .csv files
+airlines = pd.read_csv('airlines.csv')
+airports = pd.read_csv('airports.csv')
+flights = pd.read_csv('flights.csv')
+
+# display a concise summary of dataframes
+airlines.info()
+airports.info()
+flights.info()
+
+# select a sample of 100 rows from all data available 
+# defining a seed for analysis purposes
+flights_interest = flights.sample(n= 100, random_state= 7)
+
+# let's check some rows of sample
+flights_interest
